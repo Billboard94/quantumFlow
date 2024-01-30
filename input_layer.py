e604b819598980fc1957e669df43dbaae86ebc03
Valmir <valmir.ganiu@tu-dotrmund.de>
2024-01-30 20:45:23 +0100

    input layer example created by perplexity ai

---
+++ input_layer.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+# Example input features
+initial_conditions = np.array([[1.0], [2.0]])
+boundary_conditions = np.array([[3.0], [4.0]])
+material_properties = {'electron_mass': 0.000511, 'charge': -1}
